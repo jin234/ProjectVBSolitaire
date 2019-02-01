e481c4b7bf4a688f986a67bbc2dfa00e4c06f507
Savist Pingkarawat <setsuna_F_seiei-0000@hotmail.com>
2019-02-01 19:54:29 +0900

    Change Name Main Game to MainGame

---
+++ VBSolitaire/VBSolitaire/MainGame.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim btnBacktoMain As New Menu
+        btnBacktoMain.Show()
+    End Sub
+End Class
--- VBSolitaire/VBSolitaire/Menu.vb
+++ VBSolitaire/VBSolitaire/Menu.vb
@@ -2,4 +2,8 @@
     Private Sub Menu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub btnNewGame_Click(sender As Object, e As EventArgs) Handles btnNewGame.Click
+
+    End Sub
 End Class
