447aec0a8ce0ea25b143d472d8189c9bcafd9382
jin234 <ka.jirapat@gmail.com>
2019-02-04 21:32:13 +0700

    Revent change

--- VBSolitaire/VBSolitaire/MainGame.vb
+++ VBSolitaire/VBSolitaire/MainGame.vb
@@ -1,10 +1,6 @@
-﻿Public Class MainGame
+﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim btnBacktoMain As New Menu
         btnBacktoMain.Show()
-        Dim CloseGame As New Menu
-        CloseGame.Owner = Me
-        Me.Hide()
     End Sub
-
 End Class
